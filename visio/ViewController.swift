5cbc5e75778b8e841f46347e202d1a46963ce2d0
guruduttperi <thegdaproject@gmail.com>
2020-01-21 10:42:04 -0500

    Camera

--- visio/ViewController.swift
+++ visio/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // Getting Device Input
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else{
+            return
+        }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else{
+            return
+        }
+        
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        // Getting Camera Output
+        
+        let cameraOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(cameraOutput)
+        
+        
+        // Displaying output
+        
+        let cameraPreview = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreview.frame = CGRect(x:0, y:0, width: cameraView.frame.size.width, height: cameraView.frame.size.height)
+        cameraView.layer.addSublayer(cameraPreview)
+        
+        
+        
+        
     }
 
 
